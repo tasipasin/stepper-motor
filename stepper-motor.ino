43d298cdabcbcefa71b26f74bf9f8bbc59cf835d
Tasi Pasin <tasi.pasin96@gmail.com>
2023-10-07 18:03:44 -0300

    Corrigido processo para girar o motor

--- stepper-motor.ino
+++ stepper-motor.ino
@@ -1,58 +1,66 @@
-#include <ArduinoJson.h>
 // Inclusões do motor de passo
 #include <AccelStepper.h>
+#include <math.h>
 
-#define MOTOR_YELLOW_PIN D0
-#define MOTOR_ORANGE_PIN D1
-#define MOTOR_RED_PIN D2
-#define MOTOR_BROWN_PIN D3
+#define MOTOR_YELLOW_PIN 4
+#define MOTOR_ORANGE_PIN 0
+#define MOTOR_RED_PIN 2
+#define MOTOR_BROWN_PIN 15
 
 // Define full step 4 wire
 AccelStepper stepperMotor(AccelStepper::FULL4WIRE, MOTOR_BROWN_PIN, MOTOR_ORANGE_PIN, MOTOR_RED_PIN, MOTOR_YELLOW_PIN);
-// Number of steps per internal motor revolution 
-const float STEPS_PER_REV = 32; 
+// Number of steps per internal motor revolution
+const float STEPS_PER_REV = 32;
 //  Amount of Gear Reduction
 const float GEAR_RED = 64;
 // Number of steps per geared output rotation
 const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
 
+const float CM_PER_REV = 3.1;
+
+const float TIME_TO_WALK = 10;
+
 // 34,13 steps/seg
 
-// Define o pino de leitura do botão
-#define BUTTON_PIN D6
-
-// Variáveis de teste
-int position;
+bool spin = true;
+float debugAcc = 0;
+bool ended = false;
 
 /**
   Step Motor Initial Setup
 */
 void setupstepperMotor() {
-  stepperMotor.setMaxSpeed(500);
-}
-
-void printPosition() {
-  String result;
-  DynamicJsonDocument doc(200);  // allocates in the heap
-  doc["position"] = position;
-  serializeJson(doc, Serial);
-  Serial.print("\n");
+  stepperMotor.setMaxSpeed(999);
 }
 
 void setup() {
   Serial.begin(9600);
-  Serial.print("Inicio\n\n\n\n");
-  // initialize the pushbutton pin as an pull-up input
-  pinMode(BUTTON_PIN, INPUT);
+
   setupstepperMotor();
-  position = 0;
 }
 
 void loop() {
-  // Meia volta
-  int stepsRequired = STEPS_PER_OUT_REV / 2;
-  int rotBySec = 100 * STEPS_PER_OUT_REV
-  stepperMotor.setSpeed(100);
-  stepperMotor.step(stepsRequired);
-  sleep(stepsRequired / 100);
+  if (spin) {
+    // Indica quantos centímetros devem ser enrolados/soltos
+    float cmPull = 20;
+    // Realiza cálculo de quantas voltas são necessárias
+    //float revolutions = cmPull / CM_PER_REV;
+    float revolutions = 1;
+    // Realiza cálculo de quantidade de passos necessários
+    long stepsToTake = round(revolutions * STEPS_PER_OUT_REV);
+    Serial.print("Passos necessários: "); Serial.println(stepsToTake);
+    // Calcula a velocidade necessária para realizar as voltas no tempo determinado
+    int speedNeeded = round(stepsToTake / TIME_TO_WALK);
+    Serial.print("Velocidade calculada: "); Serial.println(speedNeeded);
+    Serial.println("Começou");
+    stepperMotor.setSpeed(speedNeeded);
+    stepperMotor.setAcceleration(50);
+    stepperMotor.move(stepsToTake);
+    spin = false;
+  }
+  stepperMotor.run();
+  if (!stepperMotor.isRunning() && !ended) {
+    ended = true;
+    Serial.println("Acabou");
+  }
 }
