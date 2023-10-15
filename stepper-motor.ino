99e384ae0b40d777628ea22b022db6574a28621c
Tasi Pasin <tasi.pasin96@gmail.com>
2023-10-15 14:28:13 -0300

    - Corrigido código de controle do motor de passo

--- stepper-motor.ino
+++ stepper-motor.ino
@@ -1,66 +1,56 @@
 // Inclusões do motor de passo
-#include <AccelStepper.h>
+#include <Stepper.h>
 #include <math.h>
 
-#define MOTOR_YELLOW_PIN 4
-#define MOTOR_ORANGE_PIN 0
-#define MOTOR_RED_PIN 2
-#define MOTOR_BROWN_PIN 15
+// Definições para ESP32
+#define MOTOR_IN_1_PIN 25
+#define MOTOR_IN_2_PIN 26
+#define MOTOR_IN_3_PIN 27
+#define MOTOR_IN_4_PIN 14
 
-// Define full step 4 wire
-AccelStepper stepperMotor(AccelStepper::FULL4WIRE, MOTOR_BROWN_PIN, MOTOR_ORANGE_PIN, MOTOR_RED_PIN, MOTOR_YELLOW_PIN);
-// Number of steps per internal motor revolution
-const float STEPS_PER_REV = 32;
-//  Amount of Gear Reduction
-const float GEAR_RED = 64;
+// Definições para Arduino
+//#define MOTOR_IN_1_PIN 8
+//#define MOTOR_IN_2_PIN 9
+//#define MOTOR_IN_3_PIN 10
+//#define MOTOR_IN_4_PIN 11
+
 // Number of steps per geared output rotation
-const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
+const int STEPS_PER_OUT_REV = 2049;
+// Define full step 4 wire
+Stepper stepperMotor(STEPS_PER_OUT_REV, MOTOR_IN_1_PIN, MOTOR_IN_3_PIN, MOTOR_IN_2_PIN, MOTOR_IN_4_PIN);
 
-const float CM_PER_REV = 3.1;
+const float CM_PER_REV = 7.6;
 
-const float TIME_TO_WALK = 10;
+const float TIME_TO_WALK = 20;
 
-// 34,13 steps/seg
-
-bool spin = true;
-float debugAcc = 0;
-bool ended = false;
-
-/**
-  Step Motor Initial Setup
-*/
-void setupstepperMotor() {
-  stepperMotor.setMaxSpeed(999);
-}
-
+bool took = true;
 void setup() {
   Serial.begin(9600);
-
-  setupstepperMotor();
+  // In rotations per minutes
+  stepperMotor.setSpeed(10);
 }
-
+//35,9 43,5
 void loop() {
-  if (spin) {
+  if (took) {
     // Indica quantos centímetros devem ser enrolados/soltos
-    float cmPull = 20;
+    //float cmPull = 20;
+    int cmPull = 10;
     // Realiza cálculo de quantas voltas são necessárias
-    //float revolutions = cmPull / CM_PER_REV;
-    float revolutions = 1;
+    float revolutions = cmPull / CM_PER_REV;
+    //int revolutions = 1;
     // Realiza cálculo de quantidade de passos necessários
-    long stepsToTake = round(revolutions * STEPS_PER_OUT_REV);
+    int stepsToTake = round(revolutions * STEPS_PER_OUT_REV);
     Serial.print("Passos necessários: "); Serial.println(stepsToTake);
     // Calcula a velocidade necessária para realizar as voltas no tempo determinado
-    int speedNeeded = round(stepsToTake / TIME_TO_WALK);
+    long speedNeeded = round(stepsToTake / TIME_TO_WALK);
     Serial.print("Velocidade calculada: "); Serial.println(speedNeeded);
     Serial.println("Começou");
-    stepperMotor.setSpeed(speedNeeded);
-    stepperMotor.setAcceleration(50);
-    stepperMotor.move(stepsToTake);
-    spin = false;
-  }
-  stepperMotor.run();
-  if (!stepperMotor.isRunning() && !ended) {
-    ended = true;
+    int steps = stepsToTake;
+    delay(1000);
+    stepperMotor.step(stepsToTake);
+    delay(20000);
+    stepperMotor.step(-2*stepsToTake);
     Serial.println("Acabou");
+    took = false;
   }
 }
